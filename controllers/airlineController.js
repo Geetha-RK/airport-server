d9327f4771e38df7c1877eec809e19180dfed61d
Geetha R Kumar <geetha2work@gmail.com>
2025-04-12 23:18:21 -0400

    Build the api route for airline get all records

--- controllers/airlineController.js
+++ controllers/airlineController.js
@@ -1,7 +1,16 @@
 import client from '../connection.js'
 
+//Get all airlines - /api/airlines/
 const index = async (req,res) => {
-
+    try {
+        const result = await client.query('SELECT * FROM AIRLINES');
+        if(result.length === 0 ){
+            return res.status(400).json({message:"no airlines found "});
+        }
+        res.status(200).json(result.rows);
+    } catch (error) {
+        res.status(500).json( {error:error.message} )
+    }
 }
 
 export{ index }
